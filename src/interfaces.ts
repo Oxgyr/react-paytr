49386c5dff5445ee5d616d19a9790821355d04a3
Ubeyt <190541075@firat.edu.tr>
2020-10-23 17:42:26 +0300

    makes params optional

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -3,34 +3,34 @@ export interface IPaytrProps {
   user: IPaytrUser;
   configs: IPaytrConfigs;
   tokenEndpoint:string;
-  style:Object,
+  style?:Object,
 }
 
 export interface IPaytrUser{
-  email:string
-  name:string
-  address:string
-  phone:string
-  user_ip:string;
+  email?:string
+  name?:string
+  address?:string
+  phone?:string
+  user_ip?:string;
 }
 
 
 export interface IPaytrOrder {
-  merchant_oid:string;
+  merchant_oid?:string;
   basket:TBasket;
-  payment_amount:string
-  currency:string;
-  no_installment:string;
-  max_installment:string;
+  payment_amount?:string
+  currency?:string;
+  no_installment?:string;
+  max_installment?:string;
 }
 
 export interface IPaytrConfigs{
-  timeout_limit:string,
-  debug_on:string,
-  test_mode:string,
-  merchant_ok_url:string;
-  merchant_fail_url:string;
-  paytr_iframe_url:string
+  timeout_limit?:string,
+  debug_on?:string,
+  test_mode?:string,
+  merchant_ok_url?:string;
+  merchant_fail_url?:string;
+  paytr_iframe_url?:string
 }
 
 type product = string
